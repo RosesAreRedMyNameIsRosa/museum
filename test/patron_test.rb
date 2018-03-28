9ca7e582e0e533949409c278d5e47ac253b92aba
Richard Rosa Serrano <richardrosaserrano@gmail.com>
2018-03-28 09:43:27 -0600

    I added another test to make sure that we can really add interests to bob

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -6,8 +6,16 @@ require 'pry'
 
 class PatronTest < MiniTest::Test
 	def test_that_it_exist
-		bob = Patron.new("bob")
+		bob = Patron.new("Bob")
 		# binding.pry
-		assert_equal "bob", bob.name
+		assert_equal "Bob", bob.name
+	end
+
+	def test_that_we_can_add_interests
+		bob = Patron.new("Bob")
+		bob.add_interest("Dead Sea Scrolls")
+		bob.add_interest("Gems and Minerals")
+		assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], bob.interests
+		# binding.pry
 	end
 end
