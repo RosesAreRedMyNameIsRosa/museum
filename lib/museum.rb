b94588212c3bd27ef71590bda3649fc367b0b0b3
Richard Rosa Serrano <richardrosaserrano@gmail.com>
2018-03-28 10:18:58 -0600

    I added a new instance variable and a getter method so that we can set the revenue to dmns to 0

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,14 +1,16 @@
 class Museum
 	attr_reader :name,
 						:exhibits,
-						:cost
+						:cost,
+						:revenue
 	def initialize(name, exhibits = [], cost = [])
 		@name = name
 		@exhibits = exhibits
 		@cost = cost
+		@revenue = 0
 	end
 	def  add_exhibit(new_exhibits, cost)
-		@exhibits << new_exhibits 
+		@exhibits << new_exhibits
 		@exhibits << cost
 	end
 end
