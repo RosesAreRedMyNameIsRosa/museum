8dbaea151810429b38b5685a675dd9b9f701a3e4
Richard Rosa Serrano <richardrosaserrano@gmail.com>
2018-03-28 10:08:35 -0600

    I added a new test to see if it can hold exhibits, I made them all go into the same array

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -7,7 +7,15 @@ class MuseumTest < MiniTest::Test
 	def test_that_it_exisits
 		dmns = Museum.new("Denver Museum of Nature and Science")
 		assert_equal "Denver Museum of Nature and Science", dmns.name
-		binding.pry
+		# binding.pry
+	end
+
+	def test_it_can_have_exhibits
+				dmns = Museum.new("Denver Museum of Nature and Science")
+				dmns.add_exhibit("Dead Sea Scrolls", 10)
+				dmns.add_exhibit("Gems and Minerals", 0)
+				assert_equal ["Dead Sea Scrolls",10, "Gems and Minerals", 0], dmns.exhibits
+
 	end
 
 end
