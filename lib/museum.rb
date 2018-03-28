0b63b0b58a7ed37890f03153acc7c91ee5279a62
Richard Rosa Serrano <richardrosaserrano@gmail.com>
2018-03-28 10:07:27 -0600

    I added a new method to add exhibits and also added how much was the cost of admission to it, I put both the exhibit and the cost in the same array

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,6 +1,14 @@
 class Museum
-	attr_reader :name
-	def initialize(name)
+	attr_reader :name,
+						:exhibits,
+						:cost
+	def initialize(name, exhibits = [], cost = [])
 		@name = name
+		@exhibits = exhibits
+		@cost = cost
+	end
+	def  add_exhibit(new_exhibits, cost)
+		@exhibits << new_exhibits 
+		@exhibits << cost
 	end
 end
