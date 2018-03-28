d78e2324b67ad9f14c4ae81a51108b6a2e6194be
Richard Rosa Serrano <richardrosaserrano@gmail.com>
2018-03-28 10:13:30 -0600

    I added a new patron named sally and also gave her an interest

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -15,7 +15,15 @@ class PatronTest < MiniTest::Test
 		bob = Patron.new("Bob")
 		bob.add_interest("Dead Sea Scrolls")
 		bob.add_interest("Gems and Minerals")
-		assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], bob.interests
+		bob.add_interest("Imax")
+		assert_equal ["Dead Sea Scrolls", "Gems and Minerals","Imax"], bob.interests
 		# binding.pry
+	end
+
+	def test_that_we_can_add_a_new_patron
+		sally = Patron.new("Sally")
+		sally.add_interest("Dead Sea Scrolls")
+		assert_equal "Sally", sally.name
+		assert_equal ["Dead Sea Scrolls"], sally.interests
 	end
 end
