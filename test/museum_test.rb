c5b30b70cd7716eac782057cc5d8b92f4df66952
Richard Rosa Serrano <richardrosaserrano@gmail.com>
2018-03-28 10:37:19 -0600

    I added a new test to see if dmns can admit patrons and gain revenue that way

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -1,13 +1,13 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/museum'
+require './test/patron_test'
 require 'pry'
 
 class MuseumTest < MiniTest::Test
 	def test_that_it_exisits
 		dmns = Museum.new("Denver Museum of Nature and Science")
 		assert_equal "Denver Museum of Nature and Science", dmns.name
-		# binding.pry
 	end
 
 	def test_it_can_have_exhibits
@@ -21,6 +21,16 @@ class MuseumTest < MiniTest::Test
 	def test_how_much_revenue_dmns_has
 		dmns = Museum.new("Denver Museum of Nature and Science")
 		dmns.revenue
-		assert_equal 0,dmns.revenue
+		assert_equal 0, dmns.revenue
+	end
+
+	def test_that_dmns_can_admit_patrons
+		dmns = Museum.new("Denver Museum of Nature and Science")
+		dmns.revenue
+		dmns.admit(bob)
+		dmns.admit(sally)
+		dmns.revenue
+		assert_equal 40, dmns.revenue
+
 	end
 end
